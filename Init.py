a468863d7fa1a0e12ac2fad9fb5442e42e5215ee
Philipp M. Fischer <philipp.fischer@dlr.de>
2019-09-10 11:16:18 +0200

    Improved path correction - (Task #5)
    - Only adding user directory to path if it does not yet exist.
    
    ---
    Task #5: Implement Import Functionality

--- Init.py
+++ Init.py
@@ -27,16 +27,23 @@ import FreeCAD
 from os.path import isdir
 import sys
 
+
 # FreeCAD seems to load modules differently once they are stored in the User Home directory.
 # We try to load the whole folder if it exists
 freecad_user_home = FreeCAD.getUserAppDataDir()
 freecad_user_mod = freecad_user_home + "Mod"
 
-print("See if the directory " + freecad_user_mod + "exists...")
+Log = FreeCAD.Console.PrintLog
 
-if isdir(freecad_user_home):
-    print("Directory exists and will be appended to system path...")
-    sys.path.append(freecad_user_home)
+Log("See if the directory " + freecad_user_mod + "exists...")
+
+if isdir(freecad_user_mod):
+    Log("Directory Exists... Check if it is already on the path...")
+    if (freecad_user_mod in sys.path):
+        Log("Directory is already on the path...")
+    else:
+        Log("Directory will be appended to system path...")
+        sys.path.append(freecad_user_mod)
 
 # Finally register the unit test for being executed with all other FreeCAD tests
 FreeCAD.__unit_test__ += ["TestVirtualSatelliteApp"]
