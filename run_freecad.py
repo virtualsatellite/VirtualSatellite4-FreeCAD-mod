2b711e89e4658aca1f432d16e94e199cc9f0f354
kain-ap <kain_ap@SC-010285.intra.dlr.de>
2025-01-20 10:55:15 +0100

    Updated run_freecad.py according to os

--- run_freecad.py
+++ run_freecad.py
@@ -1,17 +1,40 @@
 import subprocess
 import os
 import sys
+import platform
 
+# Get the current working directory
 print(os.getcwd())
-current_dir=os.getcwd()
-# Base path to the FreeCAD executable
-freecad_dir = os.path.join(current_dir, "FreeCAD/squashfs-root/usr/bin")
-freecad_executable = os.path.join(freecad_dir,"freecad")
+current_dir = os.getcwd()
 
-# Base arguments (common ones can be set here)
-base_args = [
-   freecad_executable
-]
+# Detect the operating system
+def is_windows():
+    return os.name == 'nt' or platform.system().lower() == 'windows'
+
+def is_linux():
+    return os.name == 'posix' or platform.system().lower() == 'linux'
+
+if is_linux():
+    # Linux-specific FreeCAD path
+    freecad_dir = os.path.join(current_dir, "FreeCAD/squashfs-root/usr/bin")
+    freecad_executable = os.path.join(freecad_dir, "freecad")
+
+    # Base arguments
+    base_args = [
+        freecad_executable
+    ]
+
+elif is_windows():
+    # Windows-specific FreeCAD path
+    freecad_path = os.path.join(current_dir, "FreeCAD/bin/FreeCAD.exe")
+
+    # Base arguments
+    base_args = [
+        freecad_path
+    ]
+else:
+    print("Unsupported operating system.")
+    sys.exit(1)
 
 # Collect additional arguments from command-line inputs (if provided)
 additional_args = sys.argv[1:]
